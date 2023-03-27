742880b912797e75616ae7d54dff8b73987cf64f
Carlos bettoi <carlosbettoi@yahoo.com>
2023-03-27 20:43:54 -0300

    finalizando o css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header"
+import { Tasks } from "./components/Tasks"
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Tasks/>
     
     </>
   )
